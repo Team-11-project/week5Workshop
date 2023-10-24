b6dc918f4b079fe1034893fd3288105afc5c0413
Rukayat-T <tewogbaderukayat@gmail.com>
2023-10-24 11:08:38 +0100

    math class

---
+++ Math.java
@@ -0,0 +1,24 @@
+public class Math {
+
+    public int max(int a, int b) {
+        if (a > b) {
+            return a;
+        }
+        return b;
+    }
+
+    public int min(int a, int b) {
+        if (a < b) {
+            return a;
+        }
+        return b;
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+}
