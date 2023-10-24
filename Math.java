da3348a8fd323c8013d1ca7725be562009703b14
Rukayat-T <tewogbaderukayat@gmail.com>
2023-10-24 11:11:27 +0100

    added comments

--- Math.java
+++ Math.java
@@ -1,5 +1,10 @@
 public class Math {
 
+    /********************************
+     * Developer: Ruka
+     * University ID: 230357811
+     * Function: This function takes two inputs as integers and returns the max
+     ********************************/
     public int max(int a, int b) {
         if (a > b) {
             return a;
